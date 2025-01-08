f8a694f6c934a141f3965b2661191278450889b8
shandelortiz <snoeortizm@gmail.com>
2025-01-07 20:34:02 -0600

    Oepradores en JavaScript

---
+++ 03 javascript/leccion-02/02-01-TiposDatosJavaScript.js
@@ -0,0 +1,69 @@
+// STRING
+var nombre = "Shandel";
+console.log(nombre);
+
+// NUMERICO
+var numero = 1000;
+console.log(numero);
+
+// OBJECT - OBJETO
+var objeto = {
+    nombre: "Shandel",
+    apellido: "Ortiz",
+    telefono: "12345678"
+}
+console.log(objeto);
+
+// BOOLEAN (TRUE or FALSE)
+var bandera = true;
+console.log(bandera);
+
+// FUNCTION
+function miFuncion() {
+
+}
+console.log(miFuncion);
+
+// SYMBOL
+var simbolo = Symbol("Mi simbolo");
+console.log(Symbol);
+
+// CLASE
+class persona {
+    constructor(nombre, apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+console.log(persona);
+
+// UNDEFINED
+var x;
+console.log(x);
+
+// NULL (Ausencia de valor)
+var y = null;
+console.log(y);
+
+// ARRAY
+var autos = [ 'BMW', 'AUDI', 'Volvo' ];
+console.log(autos);
+
+
+
+// CONCATENAR CADENAS
+var nombre = 'Juan';
+var apellido = 'Perez';
+
+var nombreCompleto = nombre + ' ' + apellido;
+console.log(nombreCompleto);
+
+
+
+// VAR, LET y CONST - Tipos de varialbes
+
+let nombre;
+nombre = "Shandel";
+console.log(nombre);
+
+const apellido = "Perez";
