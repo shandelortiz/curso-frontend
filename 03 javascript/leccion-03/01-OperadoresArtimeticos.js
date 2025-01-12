ea8787fca267cc7ecd163d45c8135c9b0d698e56
shandelortiz <snoeortizm@gmail.com>
2025-01-12 12:33:59 -0600

    Modificacion de nombres en archivos

---
+++ 03 javascript/leccion-03/01-OperadoresArtimeticos.js
@@ -0,0 +1,50 @@
+// SUMA
+let a = 3, b = 2;
+let z = a + b;
+console.log("Resultado de la suma: " + z);
+
+// RESTA
+z = a - b;
+console.log("Resultado de la resta: " + z);
+
+// MULTIPLICACION
+z = a * b;
+console.log("Resultado de la multiplicacion: " + z);
+
+// DIVISION
+z = a / b;
+console.log("Resultado de la division: " + z);
+
+// MODULO (residuo de la division)
+z = a % b;
+console.log("Resultado de operacion modulo (residuo): " + z);
+
+// EXPONENTE
+z = a ** b;
+console.log("Resultado de operador exponente: " + z);
+
+
+// INCREMENTO
+// PRE-INCREMENTO (el operador ++ ANTES de la variable)
+z = ++a
+console.log("Pre-Incremento");
+console.log(a);
+console.log(z);
+
+// POST-INCREMENTO (el operador ++ DESPUES de la variable)
+z = b++;
+console.log("Post-Incremento");
+console.log(b);
+console.log(z);
+
+// DECREMENTO
+z = --a;
+console.log("Decremento/Predecremento");
+console.log(a);
+console.log(z);
+
+// POSTDECREMENTO
+z = b--;
+console.log("Postdecremento");
+console.log(b);
+console.log(z);
